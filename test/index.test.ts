af0abccb6a4e2f56f61dc81874e5a53d3b6fa19e
Thomas Coopman <thomas.coopman@gmail.com>
2017-05-12 10:17:57 +0200

    impossible for an illegal move

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,34 @@
 import * as test from 'tape';
 
-test('timing test', (t: test.Test) => {
-    t.plan(2);
-    
-    t.equal(typeof Date.now, 'function');
-    var start = Date.now();
-    
-    setTimeout(function () {
-        t.equal(Date.now() - start, 100);
-    }, 100);
+type Coordinate = 0 | 1 | 2;
+
+class Game {
+    isOpen() {
+        return true;
+    }
+    makeMove(line: Coordinate, column:Coordinate) {
+
+    }
+}
+
+test('Create a new game', (t: test.Test) => {
+    new Game();
+    t.end();
+});
+
+test('A new game is always open', (t: test.Test) => {
+    const game = new Game();
+    t.equal(game.isOpen(), true);
+    t.end();
+});
+
+test('Make a new legal move', (t: test.Test) => {
+    const game = new Game();
+    game.makeMove(0, 0);
+    t.end();
+});
+
+test('You cannot make an illegal move', (t: test.Test) => {
+    // Impossible by the compiler
+    t.end();
 });
