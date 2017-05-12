e0e61c8415e1e79955ba48d8d2af598b285ad428
Thomas Coopman <thomas.coopman@gmail.com>
2017-05-12 09:56:15 +0200

    initial test

---
+++ test/index.test.ts
@@ -0,0 +1,12 @@
+import * as test from 'tape';
+
+test('timing test', (t: test.Test) => {
+    t.plan(2);
+    
+    t.equal(typeof Date.now, 'function');
+    var start = Date.now();
+    
+    setTimeout(function () {
+        t.equal(Date.now() - start, 100);
+    }, 100);
+});
